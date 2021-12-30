09b3982cd16c9319ee3791bd770fb963e689b524
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-30 11:39:15 +0100

    added tests

--- napari_cupy_image_processing/_tests/test_function.py
+++ napari_cupy_image_processing/_tests/test_function.py
@@ -1,7 +1,52 @@
-# from napari_cupy_image_processing import threshold, image_arithmetic
-
-# add your tests here...
+import numpy as np
 
 
-def test_something():
-    pass
+def test_with_numpy_2d():
+    import numpy
+    image = (np.random.random((100, 100)) > 0.5) * 1
+
+    dummy_workflow(image)
+
+def dummy_workflow(image):
+    from napari_cupy_image_processing import \
+        gaussian_filter, \
+        gaussian_laplace, \
+        median_filter, \
+        percentile_filter, \
+        white_tophat, \
+        morphological_gradient, \
+        morphological_laplace, \
+        wiener, \
+        threshold_otsu, \
+        binary_fill_holes, \
+        label, \
+        black_tophat, \
+        minimum_filter, \
+        maximum_filter, \
+        binary_closing, \
+        binary_erosion, \
+        binary_opening, \
+        binary_dilation
+
+    operations = [
+        gaussian_filter,
+        gaussian_laplace,
+        median_filter,
+        percentile_filter,
+        white_tophat,
+        morphological_gradient,
+        morphological_laplace,
+        wiener,
+        threshold_otsu,
+        binary_fill_holes,
+        label,
+        black_tophat,
+        minimum_filter,
+        maximum_filter,
+        binary_closing,
+        binary_erosion,
+        binary_opening,
+        binary_dilation]
+
+    for operation in operations:
+        operation(image)
